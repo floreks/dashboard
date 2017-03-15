09234fe75c84d36fd6418008ad9521c20b1e3498
Sebastian Florek <sebastian.florek@ts.fujitsu.com>
2017-03-15 13:29:21 +0100

    Optimize travis

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -135,6 +135,9 @@ module.exports = function(config) {
 
     configuration.sauceLabs = {
       testName: testName,
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+      username: process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
       connectOptions: {port: 5757, logfile: 'sauce_connect.log'},
       public: 'public',
     },
